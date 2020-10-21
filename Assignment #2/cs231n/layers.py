db6dcd7f7734d733995404c7183c7995888cbb4e
Klawens Liu <klawens@memtrail.com>
2020-10-21 11:17:31 +0800

    batchnorm finished

--- Assignment #2/cs231n/layers.py
+++ Assignment #2/cs231n/layers.py
@@ -26,7 +26,7 @@ def affine_forward(x, w, b):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    x_reshape = np.reshape(x, (x.shape[0], -1))
+    x_reshape = x.reshape(x.shape[0], -1)
     out = np.dot(x_reshape, w) + b
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -410,7 +410,17 @@ def layernorm_forward(x, gamma, beta, ln_param):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    N, D = x.shape[0], x.shape[1]
+    mu = 1 / N * np.sum(x, axis=0)
+    xmu = x - mu
+    sq = xmu ** 2
+    var = 1 / N * np.sum(sq, axis=0)
+    sqrtvar = np.sqrt(var + eps)
+    ivar = 1 / sqrtvar
+    xhat = xmu * ivar
+    gammax = gamma * xhat
+    out = gammax + beta
+    cache = (xhat, gamma, xmu, ivar, sqrtvar, var, eps)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -445,7 +455,23 @@ def layernorm_backward(dout, cache):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    xhat, gamma, xmu, ivar, sqrtvar, var, eps = cache
+
+    N, D = dout.shape
+    dbeta = np.sum(dout, axis=0)
+    dgammax = dout
+    dgamma = np.sum(dgammax * xhat, axis=0)
+    dxhat = dgammax * gamma
+    divar = np.sum(dxhat * xmu, axis=0)
+    dxmu1 = dxhat * ivar
+    dsqrtvar = -1 / (sqrtvar ** 2) * divar
+    dvar = 0.5 * 1 / np.sqrt(var + eps) * dsqrtvar
+    dsq = 1 / N * np.ones((N, D)) * dvar
+    dxmu2 = 2 * xmu * dsq
+    dx1 = (dxmu1 + dxmu2)
+    dmu = -1 * np.sum(dxmu1 + dxmu2, axis=0)
+    dx2 = 1 / N * np.ones((N, D)) * dmu
+    dx = dx1 + dx2
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
